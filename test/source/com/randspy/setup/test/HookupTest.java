e08ca428a777e940c09f6ffb68c9da1b81ccfbe3
sgluter <mos.koziel@gmail.com>
2015-07-18 12:00:59 +0200

    Tests wired.

---
+++ test/source/com/randspy/setup/test/HookupTest.java
@@ -0,0 +1,12 @@
+package com.randspy.setup.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HookupTest {
+    @Test
+    public void nothing() throws Exception {
+        assertEquals(1, 1);
+    }
+}
