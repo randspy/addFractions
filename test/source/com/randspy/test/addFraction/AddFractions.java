de6f239ba9f57dd5b3b5ee737f5f52f2b3c7d6c5
sgluter <mos.koziel@gmail.com>
2015-07-18 15:47:37 +0200

    Test cases for negative numbers added.

--- test/source/com/randspy/test/addFraction/AddFractions.java
+++ test/source/com/randspy/test/addFraction/AddFractions.java
@@ -29,17 +29,38 @@ public class AddFractions {
 
     @Test
     public void addZeroToNumberWithDenominator() throws Exception {
-        assertEquals(new Fraction(1,2), new Fraction(0).plus(new Fraction(1,2)));
+        assertEquals(new Fraction(1, 2), new Fraction(0).plus(new Fraction(1, 2)));
     }
 
     @Test
     public void addNumberWithDenominatorToZero() throws Exception {
-        assertEquals(new Fraction(1,3), new Fraction(1,3).plus(new Fraction(0)));
+        assertEquals(new Fraction(1, 3), new Fraction(1, 3).plus(new Fraction(0)));
     }
 
     @Test
     public void addTwoNumbersWithDenominator() throws Exception {
-        assertEquals(new Fraction(5, 6), new Fraction(1,3).plus(new Fraction(1,2)));
+        assertEquals(new Fraction(5, 6), new Fraction(1, 3).plus(new Fraction(1, 2)));
+    }
+
+    @Test
+    public void addTwoNumbersWithReduction() throws Exception {
+        assertEquals(new Fraction(2, 3), new Fraction(1, 3).plus(new Fraction(3, 9)));
+    }
+
+    @Test
+    public void addNaturalNumberToNumberWithDenominator() throws Exception {
+        assertEquals(new Fraction(3, 2), new Fraction(1).plus(new Fraction(1, 2)));
+    }
+
+    @Test
+    public void addZeroToNegativeNumber() throws Exception {
+        assertEquals(new Fraction(-1,2), new Fraction(0).plus(new Fraction(-1, 2)));
+        assertEquals(new Fraction(-1,2), new Fraction(0).plus(new Fraction(1, -2)));
+    }
+
+    @Test
+    public void addNumberToNegativeNumber() throws Exception {
+        assertEquals(new Fraction(-1,4), new Fraction(1,4).plus(new Fraction(-1, 2)));
     }
 
 }
